c1d302ce69522ede3fdd82a2f2ee5380010d4818
= <piliang@nate.com>
2020-04-07 16:28:45 +0900

    이더리움 서버 블록 및 가스 가격 모니터링

---
+++ etc/zabbix/scripts/eth_gasPrice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hexstring=$(curl --silent -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":67}' 127.0.0.1:port | jq .result)
+python -c 'print(int('$hexstring',16))'
